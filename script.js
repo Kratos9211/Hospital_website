e4bfd1491f55429d8862ede203a0e6a18d2559ba
Dhaman Kovachi <dhamankovachi1@gmail.com>
2023-10-04 18:06:44 +0530

    File modification

---
+++ script.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.animation-element', {
+    duration: 1000,        // Animation duration in milliseconds
+    origin: 'bottom',      // Starting animation direction (top, right, bottom, left)
+    distance: '50px',      // Animation distance
+    delay: 0,              // Delay before the animation starts
+    easing: 'ease-in-out', // Animation easing function
+    reset: true            // Reset animation when it's not in the viewport
+});
